9e8161cac3d13a1fd90270bda51532fcc72ae826
Daggametech <dbecho@email.neit.edu>
2026-04-14 10:27:51 -0400

    edit
    working

--- WEEK 2/Level1MB/animation.js
+++ WEEK 2/Level1MB/animation.js
@@ -5,7 +5,7 @@
 // var timer;
 var interval = 1000/60;
 
-var ball = new ball();
+var ball = new GameObject();
 	// canvas = document.getElementById("canvas");
 	// context = canvas.getContext("2d");	
 
