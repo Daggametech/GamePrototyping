74763679eff00599bbe33cbea30049528aa2ad67
Daggametech <dbecho@email.neit.edu>
2026-05-12 16:45:48 -0400

    level 6 Boss
    with help

---
+++ WEEK 7/Level6BM/Level_6_Boss/js/basic_platformer.js
@@ -0,0 +1,240 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+var interval;
+var player;
+var hit = 0;
+var dodge = 0;
+
+
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+
+	player = new GameObject({x:300, y:canvas.height/2-100});
+
+	platform0 = new GameObject();
+		platform0.width = canvas.width;
+		platform0.x = canvas.width/2;
+		platform0.y = canvas.height;
+		platform0.color = "#66ff33";
+		
+	// obstical0 = new GameObject();
+	// 	obstical0.width = player.width/2;
+	// 	obstical0.height = player.height;
+	// 	obstical0.x = player.x;
+	// 	obstical0.y = -100;
+	// 	obstical0.vy = 4;
+	// 	obstical0.color = "#faa70d";
+
+
+	// platform1 = new GameObject();
+	// 	platform1.x = 500;
+	// 	platform1.y = platform0.y- platform0.height/2 - platform1.height/2;
+	// 	platform1.color = "#ffff00";
+		
+	platform1 = new GameObject();
+		platform1.width = 200;
+		platform1.height = 50;
+		platform1.x = 400;
+		platform1.color = "#66ff33";
+		platform1.y = platform0.y - 200;
+		platform1.color = "#66ff33";
+		platform1.vx = -4;
+
+		
+	
+	// goal = new GameObject({width:24, height:50, x:700, y:platform0.y-100, color:"#00ffff"});
+	
+
+	var fX = .85;
+	var fY = .97;
+	
+	var gravity = 1;
+	var gameOver = false;
+
+	interval = 1000/60;
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	
+	context.clearRect(0,0,canvas.width, canvas.height);	
+
+	if (!gameOver) {
+		if (w && player.canJump && player.vy == 0) {
+			player.canJump = false;
+			player.vy += player.jumpHeight;
+		}
+
+		if (a) {
+			player.vx += -player.ax * player.force;
+		}
+		if (d) {
+			player.vx += player.ax * player.force;
+		}
+	}
+	
+
+	player.vx *= fX;
+	player.vy *= fY;
+	
+	player.vy += gravity;
+	
+	player.x += Math.round(player.vx);
+	player.y += Math.round(player.vy);
+	
+	// platform1.x += platform1.vx;
+
+	while(platform0.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform0.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		player.vx = 0;
+	}
+	while(platform0.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		player.vx = 0;
+	}
+	while(platform0.hitTestPoint(player.top()) && player.vy <=0)
+	{
+		player.y++;
+		player.vy = 0;
+	}
+	while(platform0.hitTestPoint(player.bottomleft()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform0.hitTestPoint(player.bottomright()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	// if(obstical0.hitTestObject(player)){
+	// 	obstical0.vy = 0;
+	// 	hit++;
+	// }
+	
+	// if (obstical0.y > 900)
+	// {
+	// 	obstical0.y = -100;
+	// 	obstical0.x = player.x;
+
+	// }
+	
+	// while(platform1.hitTestPoint(player.top()) && player.vy <=0)
+	// {
+	// 	player.y++;
+	// 	player.vy = 0;
+	// 	player.canJump = true;
+	// }
+	while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+		console.log("Platform1");
+	}
+	// while(platform1.hitTestPoint(player.left()) && player.vx <=0)
+	// {
+	// 	player.x++;
+	// 	player.vx = 0;
+	// }
+	// while(platform1.hitTestPoint(player.right()) && player.vx >=0)
+	// {
+	// 	player.x--;
+	// 	player.vx = 0;
+	// }
+	// while(platform1.hitTestPoint(player.topleft()) && player.vy <=0)
+	// {
+	// 	//player.y++;
+	// 	player.vy = 0;
+	// 	player.canJump = true;
+	// }
+	// while(platform1.hitTestPoint(player.topright()) && player.vy <=0)
+	// {
+	// 	//player.y++;
+	// 	player.vy = 0;
+	// 	player.canJump = true;
+	// }
+	while(platform1.hitTestPoint(player.bottomleft()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform1.hitTestPoint(player.bottomright()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+
+	if (platform1.x < -100)
+	{
+		platform1.x = 1500;
+	}
+
+	// if(platform1.hitTestPoint(player.right()) && player.vx >=0)
+	// {
+	// 	platform1.x = 10000;
+		
+	// }
+	
+	//---------Objective: Let Me Out!---------------------------------------------------------------------------------------------------- 
+	//---------Run this program first.
+	//---------Write a condition so that the player opens the yellow door to get the pearl-----------------------------------------
+
+	
+	
+
+	// if(player.hitTestObject(goal))
+	// {
+	// 	goal.y = 10000;
+	// }
+	
+
+
+
+	
+
+	
+	
+	
+	
+	
+	platform0.drawRect();
+	// platform2.drawRect();
+	platform1.drawRect();
+	platform1.move();
+	player.drawRect();
+	// obstical0.drawRect();
+	// obstical0.move();
+
+	// if (hit > 0)
+	// {
+	// 	context.font = "100px Georgia";
+	// 	context.fillText("Game Over", canvas.width/2 - 250, canvas.height/2);
+	// 	if(!gameOver){
+	// 		gameOver = true;
+	// 	}
+	// 	gameOver = true;
+	// }
+
+
+	//Show hit points
+	player.drawDebug();
+	platform1.drawDebug();
+	// goal.drawCircle();
+}
+
